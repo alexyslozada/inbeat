33bba1c9ab6054dafe1cf97f1fd40165ce241d8d
Alexys Lozada <alexyslc@gmail.com>
2022-05-30 17:06:41 -0500

    Add test for doRequest

--- backend/usecase_test.go
+++ backend/usecase_test.go
@@ -1,6 +1,11 @@
 package backend
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestUseCase_cleanUserName(t *testing.T) {
 	table := []struct {
@@ -17,6 +22,37 @@ func TestUseCase_cleanUserName(t *testing.T) {
 			got := cleanUserName(tt.userName)
 			if got != tt.want {
 				t.Fatalf("Got %s, Want %s", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUseCase_doRequest(t *testing.T) {
+	table := []struct {
+		name       string
+		statusCode int
+		wantErr    error
+		want       string
+	}{
+		{name: "normal", statusCode: http.StatusOK, wantErr: nil, want: "ok"},
+		{name: "normal", statusCode: http.StatusNotFound, wantErr: fmt.Errorf("we get an unexpected status code: %d", http.StatusNotFound), want: ""},
+	}
+
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T) {
+			svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(tt.statusCode)
+				w.Write([]byte(tt.want))
+			}))
+			defer svr.Close()
+			data, err := doRequest(svr.URL)
+			if err != nil {
+				if err.Error() != tt.wantErr.Error() {
+					t.Fatalf("we want %v, got %v", tt.wantErr, err)
+				}
+			}
+			if string(data) != tt.want {
+				t.Fatalf("we want body %s, got %s", tt.want, string(data))
 			}
 		})
 	}
